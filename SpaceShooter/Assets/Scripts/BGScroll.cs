ceda50ba138dfd3b0d7d5c83a1b1770205d4480e
FULLNAM-U1TMULF\Administrator <jyk0802@naver.com>
2018-07-06 13:56:17 +0900

    Spaceshooter player & background setting

---
+++ SpaceShooter/Assets/Scripts/BGScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour {
+    public float Speed;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0, 0, -Speed);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("BGTrigger"))
+        {
+            rb.position += new Vector3(0, 0, 40.96f);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

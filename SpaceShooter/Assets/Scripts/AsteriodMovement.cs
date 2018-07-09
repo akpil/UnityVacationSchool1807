0c2c404139f05aaa679c4ac411956f684c41504c
taroszzng <taroszzng@gmail.com>
2018-07-09 13:54:34 +0900

    TirggerBox, Player Euler, Astroid, player Bolt

---
+++ SpaceShooter/Assets/Scripts/AsteriodMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteriodMovement : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float AngularForce;
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.onUnitSphere * AngularForce;
+        rb.velocity = new Vector3(0, 0, -Speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PlayerBolt"))
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+        
+    }
+}
--- SpaceShooter/Assets/Scripts/PlayerController.cs
+++ SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public Rigidbody rb;
     public float Speed;
+    public float RotateAngle;
+    public GameObject Bolt;
+    public Transform BoltPos;
+    public float FireRate;
+    private float currentRate;
+
+
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-	}
+        currentRate = 0;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,8 +25,18 @@ public class PlayerController : MonoBehaviour {
 
         rb.velocity = new Vector3(horizontal, 0, vertical) * Speed;
 
+        rb.rotation = Quaternion.Euler(new Vector3(0, 0, -horizontal * RotateAngle));
+
         rb.position = new Vector3(Mathf.Clamp(rb.position.x, -5, 5),
                                   0,
                                   Mathf.Clamp(rb.position.z, -4, 9));
+
+        if (Input.GetButton("Fire1") && currentRate <= 0)
+        {
+            GameObject temp = Instantiate(Bolt);
+            temp.transform.position = BoltPos.position;
+            currentRate = FireRate;
+        }
+        currentRate -= Time.deltaTime;
 	}
 }

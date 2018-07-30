8ac2de75ca551386240212d5e8aa832ff3316507
FULLNAM-U1TMULF\Administrator <jyk0802@naver.com>
2018-07-30 13:44:12 +0900

    Complex UI Start

---
+++ Clicker/Assets/Scripts/ShopUIElementController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopUIElementController : MonoBehaviour {
+
+    [SerializeField]
+    Text titleText,
+        contentsText,
+        purchaseText;
+    [SerializeField]
+    Button purchaseButton;
+
+	// Use this for initialization
+	void Start () {
+        SetUp("수입", "100", "100K");
+    }
+
+    public void SetUp(string title, string contents, string cost)
+    {
+        titleText.text = title;
+        contentsText.text = string.Format(contentsText.text, contents);
+        purchaseText.text = string.Format(purchaseText.text, cost);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

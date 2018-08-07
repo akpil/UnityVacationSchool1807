f43cc325ef7e9b5a65e882f7610c00dcfa5d7872
FULLNAM-U1TMULF\Administrator <jyk0802@naver.com>
2018-08-07 12:56:06 +0900

    Jump, platform

--- RunGame/Assets/Scripts/PlayerController.cs
+++ RunGame/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,24 @@ public class PlayerController : MonoBehaviour {
     private int animRunHash;
     private int animJumpHash;
     private int animDieHash;
+    private int animVeloHash;
     private Rigidbody2D rb;
     private Animator anim;
+
+    [SerializeField]
+    private float jumpVelocity;
+    [SerializeField]
+    private bool canJump;
 
     void Awake()
     {
         animRunHash = Animator.StringToHash("IsRun");
         animJumpHash = Animator.StringToHash("IsJump");
         animDieHash = Animator.StringToHash("IsDead");
+        animVeloHash = Animator.StringToHash("VerticalVelocity");
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        canJump = false;
     }
 
 	// Use this for initialization
@@ -26,14 +34,29 @@ public class PlayerController : MonoBehaviour {
     public void StartRun()
     {
         anim.SetBool(animRunHash, true);
+        canJump = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            anim.SetBool(animJumpHash, false);
+            if (anim.GetBool(animRunHash))
+            {
+                canJump = true;
+            }
+        }
     }
 
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
             anim.SetBool(animJumpHash, true);
-            rb.velocity = Vector2.up * 5;
+            rb.velocity = Vector2.up * jumpVelocity;
+            canJump = false;
         }
+        anim.SetFloat(animVeloHash, rb.velocity.y);
 	}
 }
